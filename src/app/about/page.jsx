c109ba19aaa1d9807bffa36f5ab0cde3c2570456
pavan0342 <pavankumarravuri2226@gmail.com>
2024-09-24 12:14:54 +0530

    project Done

---
+++ src/app/about/page.jsx
@@ -0,0 +1,11 @@
+
+
+export default function AboutPage() {
+    return (
+      <div>
+        <h1>About Us</h1>
+        <p>This is the About Us page of our website.</p>
+      </div>
+    );
+  }
+  
